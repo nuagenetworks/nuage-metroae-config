24d2039c62ab9dbbdeeca5e2d1f484340dc802d0
Mike Piecuch <michael.piecuch@nokia.com>
2018-01-19 11:53:42 -0500

    Added unit-tests for user data parser

--- tests/test_user_data_parser.py
+++ tests/test_user_data_parser.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 from levistate.user_data_parser import (UserDataParseError,
                                         UserDataParser)
@@ -11,6 +12,31 @@ VALID_USER_DATA_DIRECTORY = os.path.join(FIXTURE_DIRECTORY,
                                          'valid_user_data')
 INVALID_USER_DATA_DIRECTORY = os.path.join(FIXTURE_DIRECTORY,
                                            'invalid_user_data')
+
+PARSE_ERROR_CASES = [
+    ('no_exist.json', 'not found'),
+    ('invalid_json.json', 'Syntax error'),
+    ('invalid_yaml.yaml', 'Syntax error'),
+    ('missing_entry.json', 'Invalid entry'),
+    ('missing_entry.yaml', 'Invalid entry'),
+    ('missing_values.json', 'values'),
+    ('missing_values.yaml', 'values'),
+    ('invalid_values_1.json', 'must be a dictionary'),
+    ('invalid_values_1.yaml', 'must be a dictionary'),
+    ('invalid_values_2.json', 'must be a dictionary'),
+    ('invalid_values_2.yaml', 'must be a dictionary'),
+    ('invalid_fields_not_list_1.json', 'must be a list'),
+    ('invalid_fields_not_list_1.yaml', 'must be a list'),
+    ('invalid_fields_not_list_2.json', 'must be a list'),
+    ('invalid_fields_not_list_2.yaml', 'must be a list'),
+    ('invalid_fields_wrong_len.json', 'does not match'),
+    ('invalid_fields_wrong_len.yaml', 'does not match'),
+    ('invalid_child_missing_entry.json', 'Invalid entry'),
+    ('invalid_child_missing_entry.yaml', 'Invalid entry'),
+    ('invalid_child_multi_values.json', 'single value sets'),
+    ('invalid_child_multi_values.yaml', 'single value sets'),
+    ('invalid_group_multi_values.json', 'single value sets'),
+    ('invalid_group_multi_values.yaml', 'single value sets')]
 
 
 class TestUserDataParser(object):
@@ -45,3 +71,127 @@ class TestUserDataParser(object):
         data_pairs = parser.get_template_name_data_pairs()
 
         self.validate_valid_user_data(data_pairs)
+
+    @pytest.mark.parametrize("filename, message", PARSE_ERROR_CASES)
+    def test_read_files__invalid(self, filename, message):
+        parser = UserDataParser()
+
+        with pytest.raises(UserDataParseError) as e:
+            parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                             filename))
+
+        assert message in str(e)
+        assert filename in str(e)
+
+    def test_group__override(self):
+        parser = UserDataParser()
+        parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                                      'group_override.yaml'))
+
+        expected_data = [
+            ('Override', {'val1': 'override',
+                          '$group_1': 'test_group',
+                          'val2': 2,
+                          'val3': 3}),
+            ('Override', {'val1': 1,
+                          '$group_1': 'test_group',
+                          'val2': 2,
+                          'val3': 3})]
+
+        data_pairs = parser.get_template_name_data_pairs()
+
+        assert data_pairs == expected_data
+
+    def test_group__ordering(self):
+        parser = UserDataParser()
+        parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                                      'group_ordering.yaml'))
+
+        expected_data = [
+            ('Override', {'val1': 'override',
+                          '$group_1': 'test_group',
+                          'val2': 2,
+                          'val3': 3}),
+            ('Override', {'val1': 1,
+                          '$group_1': 'test_group',
+                          'val2': 2,
+                          'val3': 3})]
+
+        data_pairs = parser.get_template_name_data_pairs()
+
+        assert data_pairs == expected_data
+
+    def test_group__cross_file(self):
+        parser = UserDataParser()
+        parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                                      'group_cross_file.yaml'))
+        parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                                      'group_defs.yaml'))
+
+        expected_data = [
+            ('Cross file', {'val1': 'override',
+                            '$group_1': 'test_group_1',
+                            '$group_2': 'test_group_2',
+                            'val2': 2,
+                            'val3': 3,
+                            'val4': 4,
+                            'val5': 5}),
+            ('Cross file', {'val1': 1,
+                            '$group_1': 'test_group_1',
+                            '$group_2': 'test_group_2',
+                            'val2': 2,
+                            'val3': 3,
+                            'val4': 4,
+                            'val5': 5})]
+
+        data_pairs = parser.get_template_name_data_pairs()
+
+        assert data_pairs == expected_data
+
+    def test_group__loop(self):
+        parser = UserDataParser()
+        parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                                      'group_loop.yaml'))
+
+        expected_data = [
+            ('Loop', {'val1': 'override',
+                      '$group_1': 'test_group_1',
+                      'val2': 2,
+                      'val3': 3,
+                      'val4': 4,
+                      'val5': 5}),
+            ('Loop', {'val1': 1,
+                      '$group_1': 'test_group_2',
+                      'val2': 2,
+                      'val3': 3,
+                      'val4': 4,
+                      'val5': 5})]
+
+        data_pairs = parser.get_template_name_data_pairs()
+
+        assert data_pairs == expected_data
+
+    def test_group__nested(self):
+        parser = UserDataParser()
+        parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                                      'group_nested.yaml'))
+
+        expected_data = [
+            ('Template', {'template_val': 'template',
+                          '$group_1': 'child',
+                          'parent_val': 'parent',
+                          'child_val': 'child'})]
+
+        data_pairs = parser.get_template_name_data_pairs()
+
+        assert data_pairs == expected_data
+
+    def test_group__missing(self):
+        parser = UserDataParser()
+        parser.read_data(os.path.join(INVALID_USER_DATA_DIRECTORY,
+                                      'group_missing.yaml'))
+
+        with pytest.raises(UserDataParseError) as e:
+            parser.get_template_name_data_pairs()
+
+        assert "Group not_a_group not defined" in str(e)
--- user_data_parser.py
+++ user_data_parser.py
@@ -18,6 +18,7 @@ class UserDataParser(object):
     def __init__(self):
         self.data = list()
         self.groups = dict()
+        self.references = list()
 
     def read_data(self, path_or_file_name):
         """
@@ -54,14 +55,6 @@ class UserDataParser(object):
     # Private functions to do the work
     #
 
-    @staticmethod
-    def _get_dict_field(data_dict, field):
-        try:
-            return get_dict_field_no_case(data_dict, field)
-        except TypeError:
-            raise UserDataParseError("Invalid user data entry: " +
-                                     str(data_dict))
-
     def _read_data(self, filename):
         try:
             with open(filename, 'r') as file:
@@ -71,7 +64,9 @@ class UserDataParser(object):
 
     def _parse_data_string(self, user_data_string, filename="(internal)"):
         data_dict = self._decode_to_dict(user_data_string, filename)
-        self._parse_data_entries(data_dict)
+        reader = Reader(self)
+        reader.set_filename(filename)
+        reader.read(data_dict)
 
     def _decode_to_dict(self, user_data_string, filename):
         try:
@@ -85,104 +80,244 @@ class UserDataParser(object):
                                      (filename, lineno, str(e)))
         return data_dict
 
-    def _parse_data_entries(self, data_dict, parent_values={}):
-        if type(data_dict) == list:
-            for template in data_dict:
-                self._parse_data_entry(template, parent_values)
-        else:
-            self._parse_data_entry(data_dict, parent_values)
+    def _add_data(self, name, values_list):
+        for entry in values_list:
+            self.data.append((name, entry))
 
-    def _parse_data_entry(self, data_dict, parent_values):
-        template = UserDataParser._get_dict_field(data_dict, 'template')
-        group = UserDataParser._get_dict_field(data_dict, 'group')
+    def _add_group(self, name, values_dict):
+        self.groups[name] = values_dict
+
+    def _add_reference(self, name, values_dict):
+        self.references.append(Reference(name, values_dict))
+
+    def _resolve_groups(self):
+        for reference in self.references:
+            reference.resolve(self.groups)
+
+
+#
+# Private classes to do the work
+#
+
+class Reference(object):
+    def __init__(self, name, target_dict):
+        self.name = name
+        self.target_dict = target_dict
+
+    def resolve(self, groups):
+        if self.name not in groups:
+            raise UserDataParseError("Group %s not defined" % self.name)
+
+        group_dict = groups[self.name]
+        for key, value in group_dict.iteritems():
+            if key not in self.target_dict:
+                self.target_dict[key] = value
+
+
+class Reader(object):
+
+    def __init__(self, store, parent=None):
+        self.store = store
+        self.parent = parent
+        self.values_list = []
+        self.inherit_values = None
+        self.filename = None
+
+    def read(self, data):
+        self.read_children(data)
+
+    def new(self, data_dict):
+        template = self.get_dict_field(data_dict, 'template')
+        group = self.get_dict_field(data_dict, 'group')
 
         if template is not None:
-            values_list = self._parse_entry_contents(data_dict,
-                                                     parent_values)
-            self._add_data(template, values_list)
+            entry = TemplateReader(template, self.store, self)
         elif group is not None:
-            values_list = self._parse_entry_contents(data_dict,
-                                                     parent_values)
-            self._add_group(group, values_list)
+            entry = GroupReader(group, self.store, self)
         else:
-            raise UserDataParseError("Invalid user data entry: " +
-                                     str(data_dict))
+            self.raise_error("Invalid entry: " + str(data_dict))
 
-        self._parse_children(data_dict, values_list)
+        entry.set_filename(self.filename)
+        entry.read(data_dict)
 
-    def _parse_entry_contents(self, entry_dict, parent_values):
-        values = UserDataParser._get_dict_field(entry_dict, 'values')
+        return entry
+
+    def get_dict_field(self, data_dict, field):
+        try:
+            return get_dict_field_no_case(data_dict, field)
+        except TypeError:
+            self.raise_error("Invalid entry: " + str(data_dict))
+
+    def set_filename(self, filename):
+        self.filename = filename
+
+    def raise_error(self, message):
+        filename = ''
+        if self.filename is not None:
+            filename = self.filename + ": "
+
+        raise UserDataParseError(filename + message)
+
+    def read_values(self, data_dict):
+        values = self.get_dict_field(data_dict, 'values')
         if values is None:
-            raise UserDataParseError(
-                "Missing required entry field 'values'")
+            self.raise_error("Missing required entry field 'values'")
 
-        fields = UserDataParser._get_dict_field(entry_dict, 'fields')
+        fields = self.get_dict_field(data_dict, 'fields')
 
         if fields is None:
-            values_list = self._parse_values_list(values)
+            entry = ValuesReader(self.store, self.parent)
         else:
-            values_list = self._parse_fields(fields, values)
+            entry = FieldValuesReader(fields, self.store, self.parent)
 
-        self._combine_entries(values_list, parent_values)
+        entry.set_filename(self.filename)
+        self.values_list = entry.read(data_dict)
+
+    def read_children(self, data):
+        if type(data) == list:
+            for entry in data:
+                self.new(entry)
+        else:
+            self.new(data)
+
+    def fill_inherit_values(self):
+        if len(self.values_list) == 1:
+            self.inherit_values = self.values_list[0]
+        else:
+            self.raise_error(
+                "Only entries with single value sets can be inherited")
+
+
+class TemplateReader(Reader):
+
+    def __init__(self, name, store, parent=None):
+        super(TemplateReader, self).__init__(store, parent)
+        self.name = name
+
+    def read(self, data_dict):
+        self.read_values(data_dict)
+
+        self.store._add_data(self.name, self.values_list)
+
+        children = self.get_dict_field(data_dict, 'children')
+        if children is not None:
+            self.fill_inherit_values()
+            self.read_children(children)
+
+
+class GroupReader(Reader):
+
+    def __init__(self, name, store, parent=None):
+        super(GroupReader, self).__init__(store, parent)
+        self.name = name
+
+    def read(self, data_dict):
+        self.read_values(data_dict)
+
+        # Groups must have exactly one set of values
+        self.fill_inherit_values()
+
+        self.store._add_group(self.name, self.inherit_values)
+
+        children = self.get_dict_field(data_dict, 'children')
+        if children is not None:
+            self.read_children(children)
+
+
+class ValuesReader(Reader):
+    def __init__(self, store, parent=None):
+        super(ValuesReader, self).__init__(store, parent)
+
+    @staticmethod
+    def combine_dicts(values_dict, parent_values):
+        for key, value in parent_values.iteritems():
+            if key not in values_dict:
+                values_dict[key] = value
+
+    def read(self, data_dict):
+        values = self.get_values_field(data_dict)
+
+        if type(values) == list:
+            values_list = values
+        else:
+            values_list = [values]
+
+        values_list = self.process_values_list(values_list)
         return values_list
 
-    def _parse_children(self, entry_dict, values_list):
-        children = UserDataParser._get_dict_field(entry_dict, 'children')
-        if children is not None:
-            if len(values_list) == 1:
-                self._parse_data_entries(children, values_list[0])
-            else:
-                raise UserDataParseError(
-                    "Only entries with single value sets may have children")
+    def get_values_field(self, data_dict):
+        values = self.get_dict_field(data_dict, 'values')
 
-    def _parse_values_list(self, values_entry):
-        values = list()
-        if type(values_entry) == list:
-            for entry in values_entry:
-                values.append(self._parse_values(entry))
-        else:
-            values.append(self._parse_values(values_entry))
+        if values is None:
+            self.raise_error("Missing required entry field 'values'")
 
         return values
 
-    def _parse_values(self, values_dict):
-        if type(values_dict) != dict:
-            raise UserDataParseError("Invalid values entry: " +
-                                     str(values_dict))
+    def process_values_list(self, values_list):
+        if self.parent is None:
+            parent_values = None
+        else:
+            parent_values = self.parent.inherit_values
 
-        return values_dict
+        if parent_values is None:
+            parent_values = dict()
 
-    def _parse_fields(self, fields, values_list):
-        values = list()
+        for values_dict in values_list:
+            if type(values_dict) != dict:
+                self.raise_error(
+                    "Values without fields specified must be a dictionary: " +
+                    str(values_dict))
 
+            ValuesReader.combine_dicts(values_dict, parent_values)
+            self.read_references(values_dict)
+
+        return values_list
+
+    def read_references(self, values_dict):
+        for key, value in values_dict.iteritems():
+            if key.lower().startswith('$group'):
+                self.store._add_reference(value, values_dict)
+
+
+class FieldValuesReader(ValuesReader):
+    def __init__(self, fields, store, parent=None):
+        super(FieldValuesReader, self).__init__(store, parent)
+        self.fields = fields
+
+    def read(self, data_dict):
+        values = self.get_values_field(data_dict)
+        values_list = self.read_fields_values(self.fields, values)
+        values_list = self.process_values_list(values_list)
+
+        return values_list
+
+    def read_fields_values(self, fields, values):
         if type(fields) != list:
-            raise UserDataParseError("Fields must be a list: " +
-                                     str(fields))
+            self.raise_error("Fields must be a list: " + str(fields))
 
-        if type(values_list) != list:
-            raise UserDataParseError(
-                "Values must be a list when fields specified: " +
-                str(values_list))
-
-        if len(values_list) == 0:
-            return values
-
-        if type(values_list[0]) == list:
-            for entry in values_list:
-                values.append(self._parse_field_value_entry(fields, entry))
-        else:
-            values.append(self._parse_field_value_entry(fields, values_list))
-
-        return values
-
-    def _parse_field_value_entry(self, fields, values):
         if type(values) != list:
-            raise UserDataParseError(
-                "Values must be a list when fields specified: " +
-                str(values))
+            self.raise_error(
+                "Values must be a list when fields specified: " + str(values))
+
+        values_list = list()
+
+        if len(values) == 0 or type(values[0]) == list:
+            for entry in values:
+                values_list.append(
+                    self.read_fields_values_entry(fields, entry))
+        else:
+            values_list.append(
+                self.read_fields_values_entry(fields, values))
+
+        return values_list
+
+    def read_fields_values_entry(self, fields, values):
+        if type(values) != list:
+            self.raise_error(
+                "Values must be a list when fields specified: " + str(values))
 
         if len(fields) != len(values):
-            raise UserDataParseError(
+            self.raise_error(
                 "Values list does not match the number of fields specified: " +
                 str(values))
 
@@ -191,32 +326,3 @@ class UserDataParser(object):
             values_dict[key] = values[i]
 
         return values_dict
-
-    def _combine_entries(self, values_list, parent_values):
-        for values_dict in values_list:
-            for key, value in parent_values.iteritems():
-                if key not in values_dict:
-                    values_dict[key] = value
-
-    def _add_data(self, name, values_list):
-        for entry in values_list:
-            self.data.append((name, entry))
-
-    def _add_group(self, name, values_list):
-        if len(values_list) != 1:
-            raise UserDataParseError(
-                "Group %s must have exactly one set of values" % name)
-        self.groups[name] = values_list[0]
-
-    def _resolve_groups(self):
-        for entry_pair in self.data:
-            value_dict = entry_pair[1]
-            group_apply_dict = dict()
-            for key, group_name in value_dict.iteritems():
-                if key.lower().startswith('$group'):
-                    if group_name not in self.groups:
-                        raise UserDataParseError(
-                            "Group %s not defined" % group_name)
-                    self._combine_entries([group_apply_dict],
-                                          self.groups[group_name])
-            self._combine_entries([value_dict], group_apply_dict)
