872394ac2f342b250f6341415681314fc674951e
Mike Piecuch <michael.piecuch@nokia.com>
2018-01-31 14:47:54 -0500

    Template get_schema returns json-schema and variable validation

--- template.py
+++ template.py
@@ -1,9 +1,17 @@
 import jinja2
 import jinja2.ext
+import json
 import os
 import yaml
 
 from util import get_dict_field_no_case
+
+JSON_SCHEMA_URL = "http://json-schema.org/draft-04/schema#"
+JSON_SCHEMA_ID_PREFIX = "urn:nuage-metro:levistate:template:"
+JSON_SCHEMA_TITLE = "Schema validator for Nuage Metro Levistate template "
+VALID_VARIABLE_TYPES = ["string", "reference", "integer", "boolean", "ipv4",
+                        "ipv6", "ipv4_or_6", "choice", "list"]
+JSON_SCHEMA_STRING_TYPES = ["string", "reference", "ipv4", "ipv6", "ipv4_or_6"]
 
 
 class TemplateError(Exception):
@@ -34,6 +42,13 @@ class UndefinedVariableError(TemplateError):
     pass
 
 
+class VariableValueError(TemplateError):
+    """
+    Exception class when a variable contains the wrong value
+    """
+    pass
+
+
 class Template(object):
     """
     Configuration template.  This class is read-only.
@@ -48,7 +63,7 @@ class Template(object):
         self.template_version = "1.0"
         self.software_type = None
         self.software_version = None
-        self.schema = None
+        self.variables = None
 
     def __str__(self):
         return self.get_name() + " template"
@@ -75,13 +90,18 @@ class Template(object):
 
     def get_schema(self):
         """
-        Returns the schema for the template variables in dictionary form.
-        Format:
-            {"schema": [{"name": "Field name",
-             "type": "Data type",
-             ...}, ...]}
+        Returns the schema for the template variables in json-schema form.
         """
-        return {"schema": self.schema}
+        schema = self._convert_variables_to_schema()
+        return json.dumps(schema, indent=2)
+
+    def validate_template_data(self, **template_data):
+        """
+        Validates that the template_data provided matches the variables schema.
+        Returns True if ok, otherwise an exception is raised.
+        """
+        self._validate_data(template_data)
+        return True
 
     #
     # Private functions to do the work
@@ -123,9 +143,12 @@ class Template(object):
             self._get_required_field(template_dict, "software-type")
         self.software_version = \
             self._get_required_field(template_dict, "software-version")
-        self.schema = \
+        self.variables = \
             self._get_required_field(template_dict, "variables")
         self._get_required_field(template_dict, "actions")
+        # Convert the variables to a schema even though we are not going to
+        # use it right now.  This validates the variables.
+        self._convert_variables_to_schema()
 
     def _get_required_field(self, template_dict, field):
         try:
@@ -136,8 +159,87 @@ class Template(object):
             pass
 
         raise TemplateParseError(
-            "Required field %s missing from template %s" % (field,
-                                                            self.filename))
+            "In template %s, Required field %s missing" % (self.filename,
+                                                           field))
+
+    def _convert_variables_to_schema(self):
+        new_schema = dict()
+        self._generate_schema_headers(new_schema)
+        self._generate_schema_properties(new_schema)
+        self._generate_schema_required(new_schema)
+
+        return new_schema
+
+    def _generate_schema_headers(self, new_schema):
+        new_schema['$schema'] = JSON_SCHEMA_URL
+        name = self.get_name()
+        new_schema['$id'] = (JSON_SCHEMA_ID_PREFIX +
+                             name.lower().replace(' ', '-'))
+        new_schema['title'] = JSON_SCHEMA_TITLE + name
+        new_schema['type'] = "object"
+
+    def _generate_schema_properties(self, new_schema):
+        props = dict()
+        new_schema['properties'] = props
+
+        for variable in self.variables:
+            self._generate_schema_property(props, variable)
+
+    def _generate_schema_property(self, props, variable):
+        name = self._get_required_field(variable, "name")
+        var_type = self._get_required_field(variable, "type")
+
+        info = dict()
+        props[name] = info
+
+        title = name.lower().replace('_', ' ')
+        title = title[0].upper() + title[1:]
+        info['title'] = title
+
+        self._validate_variable_type(var_type, name)
+
+        if var_type.lower() == "list":
+            new_info = dict()
+            info['type'] = "array"
+            info['items'] = new_info
+
+            info = new_info
+            var_type = self._get_required_field(variable, "item-type")
+
+        self._generate_schema_value(info, variable, var_type, name)
+
+    def _generate_schema_value(self, info, variable, var_type, var_name):
+        self._validate_variable_type(var_type, var_name)
+        lower_type = var_type.lower()
+
+        if lower_type in JSON_SCHEMA_STRING_TYPES:
+            info['type'] = "string"
+        elif lower_type == "choice":
+            choices = self._get_required_field(variable, "choices")
+            if type(choices) != list:
+                raise TemplateParseError(
+                    "In template %s, variable %s: choices must be a list" %
+                    (self.filename, var_name))
+            info['enum'] = choices
+        else:
+            info['type'] = lower_type
+
+    def _validate_variable_type(self, var_type, var_name):
+        lower_type = var_type.lower()
+
+        if lower_type not in VALID_VARIABLE_TYPES:
+            raise TemplateParseError(
+                "In template %s, variable %s: Invalid type %s" %
+                (self.filename, var_name, var_type))
+
+    def _generate_schema_required(self, new_schema):
+        required = list()
+        new_schema['required'] = required
+
+        for variable in self.variables:
+            if 'optional' not in variable or variable['optional'] is False:
+                name = self._get_required_field(variable, "name")
+                required.append(name)
 
     def _replace_vars_with_kwargs(self, **kwargs):
         try:
@@ -152,7 +254,7 @@ class Template(object):
             raise TemplateParseError("Syntax error in %s:%d: %s" %
                                      (self.filename, e.lineno, e.message))
         except jinja2.UndefinedError as e:
-            raise UndefinedVariableError("For template %s: Variable value %s" %
+            raise UndefinedVariableError("In template %s: Variable value %s" %
                                          (self.get_name(), e.message))
 
     def _verify_all_vars_defined(self, **kwargs):
@@ -164,6 +266,89 @@ class Template(object):
     def _parse_with_vars(self, **kwargs):
         filled_template = self._replace_vars_with_kwargs(**kwargs)
         return self._decode_to_dict(filled_template)
+
+    def _validate_data(self, data):
+        var_info = self._generate_variable_info()
+
+        for name, value in data.iteritems():
+            self._validate_against_variable_info(var_info, name, value)
+
+        self._validate_required_data(var_info, data)
+
+    def _generate_variable_info(self):
+        var_info = dict()
+
+        for variable in self.variables:
+            var_name = self._get_required_field(variable, "name")
+            var_info[var_name] = variable
+
+        return var_info
+
+    def _validate_against_variable_info(self, var_info, var_name, value):
+        if var_name in var_info:
+            var_schema = var_info[var_name]
+        else:
+            # No variable definition for given data.  Extra variables are ok.
+            return True
+
+        var_type = self._get_required_field(var_schema, "type").lower()
+
+        if var_type == "list":
+            if type(value) != list:
+                self._raise_value_error(var_name, "is not a list")
+
+            item_type = self._get_required_field(var_schema,
+                                                 "item-type").lower()
+
+            for item in value:
+                self._validate_variable_value(var_schema, var_name, item,
+                                              item_type)
+        else:
+            self._validate_variable_value(var_schema, var_name, value,
+                                          var_type)
+
+        return True
+
+    def _validate_variable_value(self, var_schema, var_name, value, var_type):
+        if var_type in JSON_SCHEMA_STRING_TYPES:
+            if isinstance(value, basestring):
+                return True
+            else:
+                self._raise_value_error(var_name, "is not a string")
+        elif var_type == "integer":
+            if type(value) == int:
+                return True
+            else:
+                self._raise_value_error(var_name, "is not an integer")
+        elif var_type == "boolean":
+            if value is True or value is False:
+                return True
+            else:
+                self._raise_value_error(var_name, "is not a boolean")
+        elif var_type == "choice":
+            choices = self._get_required_field(var_schema, "choices")
+            if value in choices:
+                return True
+            else:
+                self._raise_value_error(var_name, "is not a valid choice")
+
+    def _validate_required_data(self, var_info, data):
+        missing = []
+        for var_name, var_schema in var_info.iteritems():
+            optional = get_dict_field_no_case(var_schema, "optional")
+            if optional is not True and var_name not in data:
+                missing.append(var_name)
+
+        if len(missing) == 0:
+            return True
+        else:
+            raise UndefinedVariableError(
+                "In template %s, missing required variables: %s" %
+                (self.get_name(), ', '.join(missing)))
+
+    def _raise_value_error(self, var_name, message):
+        raise VariableValueError("In template %s, variable %s: %s" % (
+            self.get_name(), var_name, message))
 
 
 class TemplateStore(object):
