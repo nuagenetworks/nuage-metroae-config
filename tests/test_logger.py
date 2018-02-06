ed95d1dfcca2867a729a41f5e46d8cd4945eae6e
Mike Piecuch <michael.piecuch@nokia.com>
2018-02-05 23:47:27 -0500

    Added logger test

---
+++ tests/test_logger.py
@@ -0,0 +1,54 @@
+import pytest
+
+from levistate.logger import Logger
+
+LOGGING_CASES = ["LOG", "ERROR", "OUTPUT", "DEBUG"]
+
+
+class TestLogger(object):
+
+    @pytest.mark.parametrize("log_type", LOGGING_CASES)
+    def test_log__single(self, log_type, capsys):
+        logger = Logger()
+        logger.set_to_stdout(log_type, False)
+
+        getattr(logger, log_type.lower())("No stdout")
+
+        logger.set_to_stdout(log_type, True)
+
+        getattr(logger, log_type.lower())("Yes stdout")
+
+        out, err = capsys.readouterr()
+
+        assert "No stdout" not in out
+        assert "Yes stdout" in out
+
+        log = logger.get()
+
+        assert log_type + ": No stdout" in log
+        assert log_type + ": Yes stdout" in log
+
+    @pytest.mark.parametrize("log_type", LOGGING_CASES)
+    def test_log__multiple(self, log_type, capsys):
+        logger = Logger()
+        logger.set_to_stdout(log_type, False)
+
+        getattr(logger, log_type.lower())("No stdout\nnext line 1")
+
+        logger.set_to_stdout(log_type, True)
+
+        getattr(logger, log_type.lower())("Yes stdout\nnext line 2")
+
+        out, err = capsys.readouterr()
+
+        assert "No stdout" not in out
+        assert "next line 1" not in out
+        assert "Yes stdout" in out
+        assert "next line 2" in out
+
+        log = logger.get()
+
+        assert log_type + ": No stdout" in log
+        assert log_type + ": next line 1" in log
+        assert log_type + ": Yes stdout" in log
+        assert log_type + ": next line 2" in log
