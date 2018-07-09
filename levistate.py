de1cec0ee6f8e34c91ce3c73d7221b3f3dde6412
Eswar Chennareddygari <eswar.chennareddygari@nokia.com>
2018-07-09 11:07:34 -0400

    Adding sub command and environment variables to levistate command line tool

--- levistate.py
+++ levistate.py
@@ -1,3 +1,4 @@
+
 import argparse
 from configuration import Configuration
 from errors import LevistateError
@@ -5,11 +6,19 @@ from logger import Logger
 from template import TemplateStore
 from user_data_parser import UserDataParser
 from vsd_writer import VsdWriter
+import os
 
 DEFAULT_VSD_USERNAME = 'csproot'
 DEFAULT_VSD_PASSWORD = 'csproot'
 DEFAULT_VSD_ENTERPRISE = 'csp'
 DEFAULT_URL = 'https://localhost:8080'
+ENV_TEMPLATE = 'TEMPLATE_PATH'
+ENV_USER_DATA = 'USER_DATA_PATH'
+ENV_VSD_USERNAME = 'VSD_USERNAME'
+ENV_VSD_PASSWORD = 'VSD_PASSWORD'
+ENV_VSD_ENTERPRISE = 'VSD_ENTERPRISE'
+ENV_VSD_URL = 'VSD_URL'
+ENV_VSD_SPECIFICATIONS = 'VSD_SPECIFICATIONS_PATH'
 
 DESCRIPTION = """This tool reads JSON or Yaml files of templates
 and user-data to write a configuration to a VSD or to revert (remove) said
@@ -18,50 +27,74 @@ configuration.  See README.md for more."""
 
 def main():
     args = parse_args()
-    levistate = Levistate(args)
+    if args.template_path is None:
+        args.template_path = []
+        args.template_path.append(os.getenv(ENV_TEMPLATE, None))
+        
+    if args.data_path is None:
+        args.data_path = []
+        args.data_path.append(os.getenv(ENV_USER_DATA, None))
+
+    if args.spec_path is None:
+        args.spec_path = []
+        args.spec_path.append(os.getenv(ENV_VSD_SPECIFICATIONS, None))
+    
+    #Check to make sure we have template path and data path set
+    if args.template_path is None or args.data_path is None:
+        print "Template path or Data path is not provided."
+        print "Please specify the template path using --tp on command line or set an environment variable TEMPLATE_PATH"
+        print "Please specify the user data path using --dp on command line or set an environment variable USER_DATA_PATH"
+    levistate = Levistate(args, args.action)
     levistate.run()
-
-
+    
 def parse_args():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+    sub_parser = parser.add_subparsers(dest='action')
+    create_parser = sub_parser.add_parser('create')
+    revert_parser = sub_parser.add_parser('revert')
+    validate_parser = sub_parser.add_parser('validate')
+    
+    add_parser_arguments(create_parser)
+    add_parser_arguments(revert_parser)
+    add_parser_arguments(validate_parser)
+    
+    return parser.parse_args()
+
+def add_parser_arguments(parser):
     parser.add_argument('-tp', '--template-path', dest='template_path',
-                        action='append', required=True,
+                        action='append', required=False,
+                        default=None,
                         help='Path containing template files')
     parser.add_argument('-sp', '--spec-path', dest='spec_path',
                         action='append', required=False,
                         help='Path containing object specifications')
     parser.add_argument('-dp', '--data-path', dest='data_path',
                         action='append', required=False,
+                        default=None,
                         help='Path containing user data')
     parser.add_argument('-t', '--template', dest='template_name',
                         action='store', required=False,
-                        default=None,
                         help='Template name')
     parser.add_argument('-d', '--data', dest='data',
                         action='append', required=False,
                         help='Specify user data as key=value')
-    parser.add_argument('-r', '--revert', dest='revert',
-                        action='store_true', required=False,
-                        help='Revert (delete) templates instead of applying')
     parser.add_argument('-v', '--vsd-url', dest='vsd_url',
                         action='store', required=False,
-                        default=DEFAULT_URL,
+                        default=os.getenv(ENV_VSD_URL, DEFAULT_URL),
                         help='URL to VSD REST API')
     parser.add_argument('-u', '--username', dest='username',
                         action='store', required=False,
-                        default=DEFAULT_VSD_USERNAME,
+                        default=os.getenv(ENV_VSD_USERNAME, DEFAULT_VSD_USERNAME),
                         help='Username for VSD')
     parser.add_argument('-p', '--password', dest='password',
                         action='store', required=False,
-                        default=DEFAULT_VSD_PASSWORD,
+                        default=os.getenv(ENV_VSD_PASSWORD, DEFAULT_VSD_PASSWORD),
                         help='Password for VSD')
     parser.add_argument('-e', '--enterprise', dest='enterprise',
                         action='store', required=False,
-                        default=DEFAULT_VSD_ENTERPRISE,
+                        default=os.getenv(ENV_VSD_ENTERPRISE, DEFAULT_VSD_ENTERPRISE),
                         help='Enterprise for VSD')
-    parser.add_argument('-dr', '--dry-run', dest='dry_run',
-                        action='store_true', required=False,
-                        help='Perform validation only')
     parser.add_argument('-lg', '--logs', dest='logs',
                         action='store_true', required=False,
                         help='Show logs after run')
@@ -75,15 +108,13 @@ def parse_args():
                         action='store_true', required=False,
                         help='Displays template user data example')
 
-    return parser.parse_args()
-
-
 class Levistate(object):
 
-    def __init__(self, args):
+    def __init__(self, args, action):
         self.args = args
         self.template_data = list()
         self.logger = Logger()
+        self.action = action
 
     def run(self):
 
@@ -173,6 +204,7 @@ class Levistate(object):
 
     def setup_template_store(self):
         self.store = TemplateStore()
+        
         for path in self.args.template_path:
             self.store.read_templates(path)
 
@@ -193,21 +225,21 @@ class Levistate(object):
             template_data = data[1]
             config.add_template_data(template_name, **template_data)
 
-        if self.args.dry_run is True:
+        if self.action == 'validate':
             validate_actions = [True]
         else:
             validate_actions = [True, False]
 
         for validate_only in validate_actions:
             self.writer.set_validate_only(validate_only)
-            if self.args.revert is True:
+            if self.action == 'revert':
                 config.revert(self.writer)
             else:
                 config.apply(self.writer)
 
             self.writer.set_validate_only(False)
 
-            if self.args.dry_run is True:
+            if self.action == 'validate':
                 print str(config.root_action)
 
 
