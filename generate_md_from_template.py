8843f82b67ad193821962e600a56e6efd70cbf90
Brian Castelli <bacastelli@pobox.com>
2019-09-19 13:29:37 -0400

    update with file name and proper text indents

--- generate_md_from_template.py
+++ generate_md_from_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
-import csv
+import os
 import re
 import sys
 import yaml
@@ -12,9 +12,6 @@ METADATA_RE = "([\s\S]*)\nactions:"
 
 def parse_args():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-
-    #parser.add_argument('md_file_dir', action='store',
-                        #help="path to the directory where the md files are stored.")
 
     parser.add_argument('template_file', action='store',
                         help="path to the template file to parse.")
@@ -27,8 +24,8 @@ def main():
     args = parse_args()
     with open(args.template_file, mode='r') as t:
         template_data = t.read()
-    template_metadata = re.search(METADATA_RE, template_data)
-    yaml_data = yaml.safe_load(template_metadata.group(1))
+    docfile_metadata = re.search(METADATA_RE, template_data)
+    yaml_data = yaml.safe_load(docfile_metadata.group(1) + "\nfile_name: " + os.path.basename(args.template_file))
     with open('feature_md.j2') as file_:
         template = Template(file_.read(), trim_blocks=True)
     print template.render(yaml_data)
