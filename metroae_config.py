1792e2472f66b0d5b06aab39f85e9d77fdbc7969
Mike Piecuch <michael.piecuch@nokia.com>
2020-06-18 16:07:21 -0400

    Prototype for generic query functionality

--- metroae_config.py
+++ metroae_config.py
@@ -9,6 +9,7 @@ import wget
 
 from nuage_metroae_config.configuration import Configuration
 from nuage_metroae_config.errors import MetroConfigError
+from nuage_metroae_config.query import Query
 from nuage_metroae_config.template import TemplateStore
 from nuage_metroae_config.user_data_parser import UserDataParser
 from nuage_metroae_config.vsd_writer import VsdWriter, SOFTWARE_TYPE
@@ -40,6 +41,7 @@ VALIDATE_ACTION = 'validate'
 CREATE_ACTION = 'create'
 REVERT_ACTION = 'revert'
 UPDATE_ACTION = 'update'
+QUERY_ACTION = 'query'
 LIST_ACTION = 'list'
 SCHEMA_ACTION = 'schema'
 EXAMPLE_ACTION = 'example'
@@ -74,6 +76,18 @@ def main():
                                            args.version):
         print VERSION_OUTPUT
         exit(0)
+    elif args.action == QUERY_ACTION:
+        if args.data_path is None and os.getenv(ENV_USER_DATA) is not None:
+            args.data_path = os.getenv(ENV_USER_DATA).split()
+
+        if (args.spec_path is None and
+                os.getenv(ENV_VSD_SPECIFICATIONS) is not None):
+            args.spec_path = os.getenv(ENV_VSD_SPECIFICATIONS).split()
+
+        if args.spec_path is None:
+            print REQUIRED_FIELDS_ERROR
+            exit(1)
+
     elif (args.action in [VALIDATE_ACTION, CREATE_ACTION, UPDATE_ACTION,
                           REVERT_ACTION]):
         if args.template_path is None and os.getenv(ENV_TEMPLATE) is not None:
@@ -132,6 +146,9 @@ def get_parser():
 
     update_parser = sub_parser.add_parser(UPDATE_ACTION)
     add_parser_arguments(update_parser)
+
+    query_parser = sub_parser.add_parser(QUERY_ACTION)
+    add_parser_arguments(query_parser)
 
     template_parser = sub_parser.add_parser(TEMPLATE_ACTION)
     template_sub_parser = template_parser.add_subparsers(dest='templateAction')
@@ -220,6 +237,9 @@ def add_parser_arguments(parser):
                         default=os.getenv(ENV_VSD_ENTERPRISE,
                                           DEFAULT_VSD_ENTERPRISE),
                         help='Enterprise for VSD. Can also set using environment variable %s' % (ENV_VSD_ENTERPRISE))
+    parser.add_argument('-q', '--query', dest='query',
+                        action='store', required=False,
+                        help='Query string to perform in query action'),
     parser.add_argument('--debug', dest='debug',
                         action='store_true', required=False,
                         help='Output in debug mode')
@@ -257,6 +277,8 @@ class MetroConfig(object):
     def __init__(self, args, action):
         self.args = args
         self.template_data = list()
+        self.query_files = list()
+        self.query_variables = dict()
         self.action = action
         self.device_version = None
 
@@ -325,17 +347,25 @@ class MetroConfig(object):
 
         self.setup_logging()
 
-        self.setup_template_store()
-        if self.list_info():
-            return
-        self.setup_vsd_writer()
-        self.parse_user_data()
-        self.parse_extra_vars()
+        if self.action == QUERY_ACTION:
+            self.setup_vsd_writer()
+            if self.args.query is None:
+                self.parse_user_data()
+        else:
+            self.setup_template_store()
+            if self.list_info():
+                return
+            self.setup_vsd_writer()
+            self.parse_user_data()
+            self.parse_extra_vars()
 
         had_error = False
         error_output = ""
         try:
-            self.apply_templates()
+            if self.action == QUERY_ACTION:
+                self.perform_query()
+            else:
+                self.apply_templates()
         except MetroConfigError as e:
             error_output = e.get_display_string()
             self.logger.error(error_output)
@@ -369,10 +399,14 @@ class MetroConfig(object):
                                         "be key=value format: " + var)
                     key = key_value_pair[0]
                     value = self.parse_var_value(key_value_pair[1])
-                    template_data[key] = value
+                    if self.action == QUERY_ACTION:
+                        self.query_variables[key] = value
+                    else:
+                        template_data[key] = value
 
-            self.template_data.append((self.args.template_name, template_data))
-            # print str(template_data)
+            if self.action != QUERY_ACTION:
+                self.template_data.append((self.args.template_name,
+                                           template_data))
 
     def parse_var_value(self, value):
         if value.lower() == "true":
@@ -496,14 +530,21 @@ class MetroConfig(object):
                                   "sure it is accessible to the docker" %
                                   (datafile))
                             exit(1)
-                    parser.read_data(datafile)
+                    if self.action == QUERY_ACTION:
+                        self.query_files.append(datafile)
+                    else:
+                        parser.read_data(datafile)
         else:
             if self.args.data_path is None or len(self.args.data_path) == 0:
                 print("Please specify a user data file or path")
                 exit(1)
             for path in self.args.data_path:
-                parser.read_data(path)
-        self.template_data = parser.get_template_name_data_pairs()
+                if self.action == QUERY_ACTION:
+                    self.query_files.append(path)
+                else:
+                    parser.read_data(path)
+        if self.action != QUERY_ACTION:
+            self.template_data = parser.get_template_name_data_pairs()
 
     def apply_templates(self):
         config = Configuration(self.store)
@@ -533,6 +574,15 @@ class MetroConfig(object):
 
             if self.action == VALIDATE_ACTION:
                 print str(config.root_action)
+
+    def perform_query(self):
+        query = Query()
+        query.set_logger(self.logger)
+        query.set_reader(self.writer)
+        if self.args.query is None:
+            for file in self.query_files:
+                query.add_query_file(file)
+        query.execute(self.args.query, **self.query_variables)
 
     def get_software_type(self):
         if self.device_version is not None:
--- nuage_metroae_config/query.py
+++ nuage_metroae_config/query.py
@@ -3,6 +3,9 @@
 # identifier[] . function()
 
 from lark import Lark, Transformer
+from logger import Logger
+import os
+
 
 query_grammer = Lark(r"""
 
@@ -14,9 +17,9 @@ query_grammer = Lark(r"""
     COMMENT       : "#" /[^\n\r]+/? _NEWLINE
     query         : _expression | assignment | _action
 
-    _expression     : retrieve | _function
+    _expression     : retrieve | _function | variable
     assignment      : CNAME "=" ( _expression | string )
-    variable        : CNAME
+    variable        : "$" CNAME
     retrieve        : attribute _dot_attribute*
     _dot_attribute  : "." attribute
     attribute       : CNAME _filter?
@@ -56,8 +59,9 @@ query_grammer = Lark(r"""
 
 class QueryExecutor(Transformer):
 
-    def __init__(self, override_variables):
+    def __init__(self, reader, override_variables):
 
+        self.reader = reader
         self.override_variables = override_variables
         self.variables = dict(override_variables)
 
@@ -66,14 +70,8 @@ class QueryExecutor(Transformer):
 
     def query(self, q):
         (result,) = q
-        if result is None:
-            pass
-        elif type(result) == dict:
-            for key in result:
-                print "%s: %s" % (key, result[key])
-        else:
-            print str(result)
-        return q[0]
+        self.log.output(self._format_result(result))
+        return result
 
     def assignment(self, t):
         (token, value) = t
@@ -84,7 +82,7 @@ class QueryExecutor(Transformer):
 
     def retrieve(self, t):
         attributes = list(t)
-        return attributes
+        return self.reader.query(attributes)
 
     def attribute(self, t):
         token = t[0]
@@ -123,15 +121,91 @@ class QueryExecutor(Transformer):
         else:
             return s.strip('"')
 
+    def _set_logger(self, logger):
+        self.log = logger
+
+    def _format_result(self, result):
+        output = ""
+        if type(result) == dict:
+            for key in result:
+                output += "%s: %s\n" % (key, self._format_result(result[key]))
+
+            output = output.strip("\n")
+        elif type(result) == list:
+            output += "["
+            output += ", ".join([self._format_result(x) for x in result])
+            output += "]"
+        elif isinstance(result, basestring):
+            output += "'"
+            output += result.replace("'", "''")
+            output += "'"
+        else:
+            output += str(result)
+
+        return output
+
 
 class Query():
 
-    def execute(self, query_text, **override_variables):
+    def __init__(self):
+        self.reader = None
+        self.query_files = list()
+        self.log = Logger()
+        self.log.set_to_stdout("ERROR", enabled=True)
 
-        tree = query_grammer.parse(query_text)
-        print tree.pretty()
-        results = QueryExecutor(override_variables).transform(tree)
+    def set_logger(self, logger):
+        self.log = logger
+
+    def set_reader(self, reader):
+        self.reader = reader
+
+    def add_query_file(self, path_or_file_name):
+        """
+        Reads and parses query sets from either all query files in a
+        directory path, or a single query file specified by filename.
+        Query files are expected to have .query extension
+        """
+        if (os.path.isdir(path_or_file_name)):
+            for file_name in os.listdir(path_or_file_name):
+                if file_name.endswith(".query"):
+                    full_path = os.path.join(path_or_file_name, file_name)
+                    self.query_files.append(full_path)
+        elif os.path.isfile(path_or_file_name):
+            self.query_files.append(path_or_file_name)
+        else:
+            raise Exception("File or path not found: " +
+                            path_or_file_name)
+
+    def execute(self, query_text=None, **override_variables):
+
+        self.reader.start_session()
+
+        if query_text is None:
+            results = list()
+            for query_file in self.query_files:
+
+                with open(query_file, "r") as f:
+                    results.append(self._perform_query(f.read(),
+                                                       override_variables))
+
+            if len(results) == 1:
+                return results[0]
+            else:
+                return results
+
+        else:
+            results = self._perform_query(query_text, override_variables)
+
+        self.reader.stop_session()
+
         return results
+
+    def _perform_query(self, query_text, override_variables):
+        tree = query_grammer.parse(query_text)
+        self.log.debug(tree.pretty())
+        qe = QueryExecutor(self.reader, override_variables)
+        qe._set_logger(self.log)
+        return qe.transform(tree)
 
 
 text = '''
@@ -150,4 +224,4 @@ domain_count = count(enterprise[*].domain[*])
 
 # print tree.pretty()
 
-print str(Query().execute(text))
+# print str(Query().execute(text))
