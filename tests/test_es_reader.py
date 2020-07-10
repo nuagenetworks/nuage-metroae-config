1c0697b7690eb3e71b0dbb0c34f79d8253f8c069
Mike Piecuch <michael.piecuch@nokia.com>
2020-07-10 11:01:52 -0400

    Unit-tests for query

--- tests/test_es_reader.py
+++ tests/test_es_reader.py
@@ -121,6 +121,117 @@ QUERY_INDEX_PAGING_CASES = [
     ({"%start": 1, "%end": 202}, ["from=1&size=100", "from=101&size=100", "from=201&size=1"], [100, 100, 1]),
 ]
 
+QUERY_NESTED_RANGE_SORT_CASES = [
+    ({}, ["a1", "a3", "a5", "a2", "a4"]),
+    ({"%end": 3}, ["a1", "a3", "a5"]),
+    ({"%end": 99}, ["a1", "a3", "a5", "a2", "a4"]),
+    ({"%end": 0}, []),
+    ({"%end": -2}, ["a1", "a3", "a5"]),
+    ({"%end": -5}, []),
+    ({"%start": 0}, ["a1", "a3", "a5", "a2", "a4"]),
+    ({"%start": 3}, ["a2", "a4"]),
+    ({"%start": 99}, []),
+    ({"%start": -1}, ["a4"]),
+    ({"%start": -2}, ["a2", "a4"]),
+    ({"%start": -6}, ["a1", "a3", "a5", "a2", "a4"]),
+    ({"%start": 0, "%end": 5}, ["a1", "a3", "a5", "a2", "a4"]),
+    ({"%start": 1, "%end": 3}, ["a3", "a5"]),
+    ({"%start": 3, "%end": 99}, ["a2", "a4"]),
+    ({"%start": 3, "%end": 4}, ["a2"]),
+    ({"%start": 3, "%end": 3}, []),
+    ({"%start": 4, "%end": 3}, []),
+    ({"%start": -5, "%end": -1}, ["a1", "a3", "a5", "a2"]),
+    ({"%start": -1, "%end": 0}, []),
+    ({"%start": -3, "%end": -1}, ["a5", "a2"]),
+    ({"%start": -3, "%end": -2}, ["a5"]),
+    ({"%start": -3, "%end": 5}, ["a5", "a2", "a4"]),
+
+    ({"%sort": "name"}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort": "name", "%end": 3}, ["a1", "a2", "a3"]),
+    ({"%sort": "name", "%end": 99}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort": "name", "%end": 0}, []),
+    ({"%sort": "name", "%end": -2}, ["a1", "a2", "a3"]),
+    ({"%sort": "name", "%end": -5}, []),
+    ({"%sort": "name", "%start": 0}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort": "name", "%start": 3}, ["a4", "a5"]),
+    ({"%sort": "name", "%start": 99}, []),
+    ({"%sort": "name", "%start": -1}, ["a5"]),
+    ({"%sort": "name", "%start": -2}, ["a4", "a5"]),
+    ({"%sort": "name", "%start": -6}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort": "name", "%start": 0, "%end": 5}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort": "name", "%start": 1, "%end": 3}, ["a2", "a3"]),
+    ({"%sort": "name", "%start": 3, "%end": 99}, ["a4", "a5"]),
+    ({"%sort": "name", "%start": 3, "%end": 4}, ["a4"]),
+    ({"%sort": "name", "%start": 3, "%end": 3}, []),
+    ({"%sort": "name", "%start": 4, "%end": 3}, []),
+    ({"%sort": "name", "%start": -5, "%end": -1}, ["a1", "a2", "a3", "a4"]),
+    ({"%sort": "name", "%start": -1, "%end": 0}, []),
+    ({"%sort": "name", "%start": -3, "%end": -1}, ["a3", "a4"]),
+    ({"%sort": "name", "%start": -3, "%end": -2}, ["a3"]),
+    ({"%sort": "name", "%start": -3, "%end": 5}, ["a3", "a4", "a5"]),
+
+    ({"%sort_asc": "name"}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort_asc": "name", "%end": 3}, ["a1", "a2", "a3"]),
+    ({"%sort_asc": "name", "%end": 99}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort_asc": "name", "%end": 0}, []),
+    ({"%sort_asc": "name", "%end": -2}, ["a1", "a2", "a3"]),
+    ({"%sort_asc": "name", "%end": -5}, []),
+    ({"%sort_asc": "name", "%start": 0}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort_asc": "name", "%start": 3}, ["a4", "a5"]),
+    ({"%sort_asc": "name", "%start": 99}, []),
+    ({"%sort_asc": "name", "%start": -1}, ["a5"]),
+    ({"%sort_asc": "name", "%start": -2}, ["a4", "a5"]),
+    ({"%sort_asc": "name", "%start": -6}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort_asc": "name", "%start": 0, "%end": 5}, ["a1", "a2", "a3", "a4", "a5"]),
+    ({"%sort_asc": "name", "%start": 1, "%end": 3}, ["a2", "a3"]),
+    ({"%sort_asc": "name", "%start": 3, "%end": 99}, ["a4", "a5"]),
+    ({"%sort_asc": "name", "%start": 3, "%end": 4}, ["a4"]),
+    ({"%sort_asc": "name", "%start": 3, "%end": 3}, []),
+    ({"%sort_asc": "name", "%start": 4, "%end": 3}, []),
+    ({"%sort_asc": "name", "%start": -5, "%end": -1}, ["a1", "a2", "a3", "a4"]),
+    ({"%sort_asc": "name", "%start": -1, "%end": 0}, []),
+    ({"%sort_asc": "name", "%start": -3, "%end": -1}, ["a3", "a4"]),
+    ({"%sort_asc": "name", "%start": -3, "%end": -2}, ["a3"]),
+    ({"%sort_asc": "name", "%start": -3, "%end": 5}, ["a3", "a4", "a5"]),
+
+    ({"%sort_desc": "name"}, ["a5", "a4", "a3", "a2", "a1"]),
+    ({"%sort_desc": "name", "%end": 3}, ["a5", "a4", "a3"]),
+    ({"%sort_desc": "name", "%end": 99}, ["a5", "a4", "a3", "a2", "a1"]),
+    ({"%sort_desc": "name", "%end": 0}, []),
+    ({"%sort_desc": "name", "%end": -2}, ["a5", "a4", "a3"]),
+    ({"%sort_desc": "name", "%end": -5}, []),
+    ({"%sort_desc": "name", "%start": 0}, ["a5", "a4", "a3", "a2", "a1"]),
+    ({"%sort_desc": "name", "%start": 3}, ["a2", "a1"]),
+    ({"%sort_desc": "name", "%start": 99}, []),
+    ({"%sort_desc": "name", "%start": -1}, ["a1"]),
+    ({"%sort_desc": "name", "%start": -2}, ["a2", "a1"]),
+    ({"%sort_desc": "name", "%start": -6}, ["a5", "a4", "a3", "a2", "a1"]),
+    ({"%sort_desc": "name", "%start": 0, "%end": 5}, ["a5", "a4", "a3", "a2", "a1"]),
+    ({"%sort_desc": "name", "%start": 1, "%end": 3}, ["a4", "a3"]),
+    ({"%sort_desc": "name", "%start": 3, "%end": 99}, ["a2", "a1"]),
+    ({"%sort_desc": "name", "%start": 3, "%end": 4}, ["a2"]),
+    ({"%sort_desc": "name", "%start": 3, "%end": 3}, []),
+    ({"%sort_desc": "name", "%start": 4, "%end": 3}, []),
+    ({"%sort_desc": "name", "%start": -5, "%end": -1}, ["a5", "a4", "a3", "a2"]),
+    ({"%sort_desc": "name", "%start": -1, "%end": 0}, []),
+    ({"%sort_desc": "name", "%start": -3, "%end": -1}, ["a3", "a2"]),
+    ({"%sort_desc": "name", "%start": -3, "%end": -2}, ["a3"]),
+    ({"%sort_desc": "name", "%start": -3, "%end": 5}, ["a3", "a2", "a1"]),
+]
+
+QUERY_NESTED_ATTR_FILTER_CASES = [
+    ({}, ["a1", "a3", "a5", "a2", "a4"]),
+    ({"avatartype": "URL"}, ["a1", "a3", "a5"]),
+    ({"avatartype": "URL", "%sort_desc": "dhcpleaseinterval"}, ["a5", "a3", "a1"]),
+    ({"avatartype": "URL", "%start": 1, "%end": 2}, ["a3"]),
+    ({"avatartype": "BASE64"}, ["a2", "a4"]),
+    ({"avatartype": "BASE64", "name": "a2"}, ["a2"]),
+    ({"avatartype": "URL", "name": "a2"}, []),
+    ({"name": ["a1", "a2", "a3"]}, ["a1", "a3", "a2"]),
+    ({"avatartype": "BASE64", "name": ["a1", "a2", "a3"]}, ["a2"]),
+]
+
+
 class TestEsReaderSession(object):
 
     def test_start__success(self):
@@ -618,3 +729,197 @@ class TestEsReaderQuery(object):
         assert len(results) == sum(num_per_call)
         if len(results) > 0:
             assert results[0] == "test"
+
+    @pytest.mark.parametrize("filter, expected", QUERY_NESTED_RANGE_SORT_CASES)
+    def test_nested_range_sort__success(self, filter, expected, requests_mock):
+        es_reader = EsReader()
+        es_reader.set_session_params(TEST_HOST, TEST_PORT)
+
+        objects = [
+            {"name": TEST_INDEX,
+             "filter": None},
+            {"name": "name",
+             "filter": filter}
+        ]
+
+        attributes = "name"
+
+        mock_object_1 = {"name": "a1"}
+        mock_object_3 = {"name": "a3"}
+        mock_object_5 = {"name": "a5"}
+        mock_object_2 = {"name": "a2"}
+        mock_object_4 = {"name": "a4"}
+
+        mock_entry = [mock_object_1, mock_object_3,
+                      mock_object_5, mock_object_2,
+                      mock_object_4]
+
+        self.register_mock_get(requests_mock, "", [mock_entry])
+
+        results = es_reader.query(objects, attributes)
+
+        assert results == expected
+
+    @pytest.mark.parametrize("filter, expected",
+                             QUERY_NESTED_ATTR_FILTER_CASES)
+    def test_nested_attr_filter__success(self, filter, expected,
+                                         requests_mock):
+        es_reader = EsReader()
+        es_reader.set_session_params(TEST_HOST, TEST_PORT)
+
+        objects = [
+            {"name": TEST_INDEX,
+             "filter": None},
+            {"name": "objects",
+             "filter": filter}
+        ]
+
+        attributes = "name"
+
+        mock_object_1 = {"name": "a1",
+                         "dhcpleaseinterval": 300,
+                         "avatartype": "URL"}
+        mock_object_3 = {"name": "a3",
+                         "dhcpleaseinterval": 400,
+                         "avatartype": "URL"}
+        mock_object_5 = {"name": "a5",
+                         "dhcpleaseinterval": 500,
+                         "avatartype": "URL"}
+        mock_object_2 = {"name": "a2",
+                         "dhcpleaseinterval": 100,
+                         "avatartype": "BASE64"}
+        mock_object_4 = {"name": "a4",
+                         "dhcpleaseinterval": 200,
+                         "avatartype": "BASE64"}
+
+        mock_entry = {"objects": [mock_object_1, mock_object_3,
+                      mock_object_5, mock_object_2,
+                      mock_object_4]}
+
+        self.register_mock_get(requests_mock, "", [mock_entry])
+
+        results = es_reader.query(objects, attributes)
+
+        assert results == expected
+
+    def test_nested__not_found(self, requests_mock):
+        es_reader = EsReader()
+        es_reader.set_session_params(TEST_HOST, TEST_PORT)
+
+        objects = [
+            {"name": TEST_INDEX,
+             "filter": None},
+            {"name": "foobar",
+             "filter": None}
+        ]
+
+        attributes = "name"
+
+        mock_object_1 = {"name": "a1",
+                         "dhcpleaseinterval": 300,
+                         "avatartype": "URL"}
+
+        mock_entry = {"objects": [mock_object_1]}
+
+        self.register_mock_get(requests_mock, "", [mock_entry])
+
+        results = es_reader.query(objects, attributes)
+
+        assert results == []
+
+    def test_nested__not_dict(self, requests_mock):
+        es_reader = EsReader()
+        es_reader.set_session_params(TEST_HOST, TEST_PORT)
+
+        objects = [
+            {"name": TEST_INDEX,
+             "filter": None},
+            {"name": "not_a_dict",
+             "filter": None}
+        ]
+
+        attributes = "name"
+
+        mock_object_1 = "not_a_dict"
+
+        mock_entry = {"objects": [mock_object_1]}
+
+        self.register_mock_get(requests_mock, "", [mock_entry])
+
+        results = es_reader.query(objects, attributes)
+
+        assert results == []
+
+    def test_nested__no_attr(self, requests_mock):
+        es_reader = EsReader()
+        es_reader.set_session_params(TEST_HOST, TEST_PORT)
+
+        objects = [
+            {"name": TEST_INDEX,
+             "filter": None},
+            {"name": "objects",
+             "filter": None}
+        ]
+
+        attributes = "foobar"
+
+        mock_object_1 = {"name": "a1",
+                         "dhcpleaseinterval": 300,
+                         "avatartype": "URL"}
+
+        mock_entry = {"objects": [mock_object_1]}
+
+        self.register_mock_get(requests_mock, "", [mock_entry])
+
+        results = es_reader.query(objects, attributes)
+
+        assert results == []
+
+    def test_nested__no_attr_multiple(self, requests_mock):
+        es_reader = EsReader()
+        es_reader.set_session_params(TEST_HOST, TEST_PORT)
+
+        objects = [
+            {"name": TEST_INDEX,
+             "filter": None},
+            {"name": "objects",
+             "filter": None}
+        ]
+
+        attributes = ["name", "foobar"]
+
+        mock_object_1 = {"name": "a1",
+                         "dhcpleaseinterval": 300,
+                         "avatartype": "URL"}
+
+        mock_entry = {"objects": [mock_object_1]}
+
+        self.register_mock_get(requests_mock, "", [mock_entry])
+
+        results = es_reader.query(objects, attributes)
+
+        assert results == [{"name": "a1"}]
+
+    def test_query_attribute__success(self):
+        es_reader = EsReader()
+
+        mock_object = {"name": "object name"}
+
+        value = es_reader.query_attribute(mock_object, "name")
+        assert value == "object name"
+
+    def test_query_attribute__not_found(self):
+        es_reader = EsReader()
+
+        mock_object = {"name": "object name"}
+
+        value = es_reader.query_attribute(mock_object, "foobar")
+        assert value is None
+
+    def test_query_attribute__bad_type(self):
+        es_reader = EsReader()
+
+        mock_object = ["name", "object name"]
+
+        value = es_reader.query_attribute(mock_object, "name")
+        assert value is None
